5a0235cdd6011562fc0e7c056f11a0c0ac7ce735
Zopherus <1012746@IHS60261436.ad.nsd.org>
2015-11-04 12:00:48 -0800

    Created the project

---
+++ Needleman–Wunsch Algorithm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Needleman_Wunsch_Algorithm
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string DNA1 = "GCATGCU";
+			string DNA2 = "GATTACA";
+			Console.WriteLine();
+			Console.ReadLine();
+		}
+
+		static void NeedlemanWunsch(string a, string b)
+		{
+		 //= new int[a.Length + 1, b.Length + 1];
+		}
+	}
+}
